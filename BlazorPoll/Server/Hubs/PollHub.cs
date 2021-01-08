2be903e1f9e32652005ffda1d9e7d55615d17a59
Michael Dumfart <michi.dumfart@gmail.com>
2021-01-08 20:55:59 +0100

    Add simple hub

---
+++ BlazorPoll/Server/Hubs/PollHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorPoll.Shared.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorPoll.Server.Hubs
+{
+    public class PollHub : Hub
+    {
+        public async Task UpdatePoll(Poll poll)
+        {
+            await Clients.All.SendAsync("UpdatePoll", poll);
+        }
+    }
+}
--- BlazorPoll/Server/Startup.cs
+++ BlazorPoll/Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using BlazorPoll.Server.Hubs;
 
 namespace BlazorPoll.Server
 {
@@ -22,13 +23,22 @@ namespace BlazorPoll.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] {"application/octet-stream"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,6 +61,7 @@ namespace BlazorPoll.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapHub<PollHub>("/pollhub");
                 endpoints.MapFallbackToFile("index.html");
             });
         }

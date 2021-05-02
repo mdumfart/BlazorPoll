7227fa910f2827222e9291aeea9176755b016a6b
Michael Dumfart <michi.dumfart@gmail.com>
2021-05-02 18:03:01 +0200

    Refactor into CenteredBaseWrapper and add login frontend

---
+++ BlazorPoll/Shared/Models/UserCredentials.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPoll.Shared.Models
+{
+    public class UserCredentials
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Username must have 3 characters at least")]
+        public string UserName { get; set; }
+        
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must have 6 characters at least")]
+        public string Password { get; set; }
+    }
+}

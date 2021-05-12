2884f5966b96d195cb57871e122dc01e6b15bf81
Michael Dumfart <michi.dumfart@gmail.com>
2021-05-12 12:39:45 +0200

    Add paginator and pagninated wrapper

---
+++ BlazorPoll/Shared/Dtos/PaginatedWrapperDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPoll.Shared.Dtos
+{
+    public class PaginatedWrapperDto<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public T Data { get; set; }
+    }
+}
